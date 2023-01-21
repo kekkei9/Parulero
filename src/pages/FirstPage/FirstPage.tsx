d33e3592e0f67e491efa8fdc37aa70776574c228
kekkei9 <kekkei2002@gmail.com>
2023-01-21 16:00:45 +0700

    fix collapse logic

--- src/pages/FirstPage/FirstPage.tsx
+++ src/pages/FirstPage/FirstPage.tsx
@@ -4,6 +4,7 @@ import './FirstPage.scss'
 import Button from '@mui/material/Button'
 import { Collapse } from '@mui/material'
 import { useSelector } from 'react-redux'
+import LightIcon from '@mui/icons-material/Light'
 import { RootState } from '../../redux/store'
 
 export interface IFirstPageProps {
@@ -21,6 +22,7 @@ const FirstPage = (props: IFirstPageProps): JSX.Element => {
   const themeColor = (bool) => ((theme === 'dark' ? !bool : bool) ? '#fff' : '#000')
 
   const toggleTheme = () => {
+    setCircleCnt((prev) => prev + 1)
     setCircleClicked((prev) => !prev)
   }
 
@@ -37,25 +39,36 @@ const FirstPage = (props: IFirstPageProps): JSX.Element => {
           style={{ backgroundColor: '#fff' }}
         />
       </div>
-      {step >= 0 && step <= 3 && (
-        <div className="flex flex-col items-center">
-          <div>
-            <div className="text-5xl mixBlendDiff">Hi there! I'm kekkei</div>
-          </div>
 
-          <Collapse in={step >= 1} className="text-3xl mixBlendDiff">
-            Oh!
+      {/* Gretting 1 */}
+      <Collapse in={step <= 3}>
+        <div className="flex flex-col items-center mixBlendDiff text-3xl">
+          <Collapse in={step >= 0} className="text-5xl">
+            Hi there! I'm kekkei
           </Collapse>
-          <Collapse in={step >= 2} className="text-3xl mixBlendDiff">
-            {theme === 'dark' ? "It's kinda flashy here" : "It's a little dark here"}
-          </Collapse>
-          <Collapse in={step >= 3} className="text-3xl mixBlendDiff">
-            {`Click the circle or 'Next' to ${theme === 'dark' ? 'turn off the light' : 'light it up'}`}
+
+          <Collapse in={step >= 1}>Oh!</Collapse>
+          <Collapse in={step >= 2}>{theme === 'dark' ? "It's kinda flashy here" : "It's a little dark here"}</Collapse>
+          <Collapse in={step >= 3}>
+            <div className="flex flex-col items-center">
+              <div>{`Click the light bulb or 'Next' to ${theme === 'dark' ? 'turn off the light' : 'light it up'}`}</div>
+              <LightIcon className="change-theme-btn" onClick={toggleTheme} />
+              <Collapse in={circleClicked}>{circleCnt >= 1 && circleCnt <= 3 ? 'Okay! Click next to continue' : 'Okay !!! Click Next please'}</Collapse>
+              <Collapse in={!circleClicked}>{circleCnt >= 1 && circleCnt <= 3 ? 'Click the light bulb or next to continue' : 'Okay then click the light bulb'}</Collapse>
+            </div>
           </Collapse>
         </div>
-      )}
-      <Collapse in={step === 3} className="text-3xl">
-        <div className="change-theme-btn" style={{ backgroundColor: `${themeColor(true)}` }} onClick={toggleTheme} onKeyDown={toggleTheme} role="presentation" />
+      </Collapse>
+
+      {/* Greeting2 */}
+
+      <Collapse in={step <= 7}>
+        <div className="flex flex-col items-center text-3xl mixBlendDiff">
+          <Collapse in={step >= 4} className="text-5xl">
+            Next is to choose your music genre
+          </Collapse>
+          <Collapse in={step >= 5}>Now choose 3 out of 6 pictures below</Collapse>
+        </div>
       </Collapse>
 
       <div className="flex flex-row m-5">
